6a0ee010d5e97139732d20151a8897de1ebb0d03
Jason Breedlove <j@jasonbreedlove.com>
2024-07-30 19:55:47 -0700

    change sqlite to postgres

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ else:
 if not database_url:
     raise RuntimeError("DATABASE_URL environment variable not set")
 
+# Ensure the correct dialect is used
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
